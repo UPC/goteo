8b2ea3b9f10cb16124a5e557ed48cdabdffd5a02
Alex Muntada <alex.muntada@upc.edu>
2018-06-04 17:16:37 +0200

    Hide about, discover and faq in main menu

--- src/Goteo/Application/EventListener/SessionListener.php
+++ src/Goteo/Application/EventListener/SessionListener.php
@@ -158,9 +158,9 @@ class SessionListener extends AbstractListener {
         Session::store('currency', $currency); // depending on request
 
         // Default menus
-        Session::addToMainMenu('<i class="icon icon-drop"></i> ' . Text::get('regular-header-about'), '/about', 'about', 10);
-        Session::addToMainMenu('<i class="fa fa-search"></i> ' . Text::get('regular-discover'), '/discover', 'discover', 20);
-        Session::addToMainMenu('<i class="fa fa-question-circle"></i> ' . Text::get('regular-faq'), '/faq', 'faq', 100);
+        //Session::addToMainMenu('<i class="icon icon-drop"></i> ' . Text::get('regular-header-about'), '/about', 'about', 10);
+        //Session::addToMainMenu('<i class="fa fa-search"></i> ' . Text::get('regular-discover'), '/discover', 'discover', 20);
+        //Session::addToMainMenu('<i class="fa fa-question-circle"></i> ' . Text::get('regular-faq'), '/faq', 'faq', 100);
 
         // Channels
         $nodes = [];
