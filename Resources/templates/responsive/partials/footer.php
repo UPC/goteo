6daca296679f81f6551c4dbe7618b0c917671b6e
Alex Muntada <alex.muntada@upc.edu>
2018-06-05 18:36:09 +0200

    Enhance chat login and user details

--- Resources/templates/responsive/partials/footer.php
+++ Resources/templates/responsive/partials/footer.php
@@ -1,10 +1,58 @@
 <footer id="footer" class="footer">
     <div class="container-fluid">
-      <?php if ($this->get_user()) { ?>
+<?php
+use Goteo\Application\Config;
+
+$user = $this->get_user();
+if ($user) {
+    $encodedChatId = Config::get('chatbro.id');
+    $siteDomain = 'nexus24.dakara.ac.upc.edu';
+    $siteUserExternalId = $user->id;
+    $siteUserFullName = $user->name;
+    $chat_secret = Config::get('chatbro.secret');
+    $siteUserProfileUrl = 'https://nexus24.dakara.ac.upc.edu/user/profile/' . $user->id;
+    $siteUserAvatarUrl = $user->avatar->getLink(56, 56, true);
+    $signature = md5(
+        $siteDomain
+    .   $siteUserExternalId
+    .   $siteUserFullName
+    .   $siteUserAvatarUrl
+    .   $siteUserProfileUrl
+    .   Config::get('chatbro.secret')
+    );
+?>
       <div class="row">
-          <script id="chatBroEmbedCode">/* Chatbro Widget Embed Code Start */function ChatbroLoader(chats,async){async=!1!==async;var params={embedChatsParameters:chats instanceof Array?chats:[chats],lang:navigator.language||navigator.userLanguage,needLoadCode:'undefined'==typeof Chatbro,embedParamsVersion:localStorage.embedParamsVersion,chatbroScriptVersion:localStorage.chatbroScriptVersion},xhr=new XMLHttpRequest;xhr.withCredentials=!0,xhr.onload=function(){eval(xhr.responseText)},xhr.onerror=function(){console.error('Chatbro loading error')},xhr.open('GET','//www.chatbro.com/embed.js?'+btoa(unescape(encodeURIComponent(JSON.stringify(params)))),async),xhr.send()}/* Chatbro Widget Embed Code End */ChatbroLoader({encodedChatId: '128iv'});</script>
+<script id="chatBroEmbedCode">
+/* Chatbro Widget Embed Code Start */
+function ChatbroLoader(chats,async) {
+    async= !1 !== async;
+    var params = {
+        embedChatsParameters: chats instanceof Array ? chats : [chats],
+        lang: navigator.language || navigator.userLanguage,
+        needLoadCode: 'undefined' == typeof Chatbro,
+        embedParamsVersion: localStorage.embedParamsVersion,
+        chatbroScriptVersion: localStorage.chatbroScriptVersion
+    };
+    var xhr = new XMLHttpRequest;
+    xhr.withCredentials = !0;
+    xhr.onload = function () { eval(xhr.responseText) };
+    xhr.onerror = function () { console.error('Chatbro loading error') };
+    xhr.open('GET','//www.chatbro.com/embed.js?'+btoa(unescape(encodeURIComponent(JSON.stringify(params)))), async);
+    xhr.send();
+}
+/* Chatbro Widget Embed Code End */
+ChatbroLoader({
+    encodedChatId: '<?= $encodedChatId ?>',
+    siteDomain: '<?= $siteDomain ?>',
+    siteUserExternalId: '<?= $siteUserExternalId ?>',
+    siteUserFullName: '<?= $siteUserFullName ?>',
+    siteUserProfileUrl: '<?= $siteUserProfileUrl ?>',
+    siteUserAvatarUrl: '<?= $siteUserAvatarUrl ?>',
+    signature: '<?= $signature ?>'
+});
+</script>
       </div>
-      <?php } ?>
+<?php } ?>
       <div class="row">
 
           <div class="col-sm-5">
