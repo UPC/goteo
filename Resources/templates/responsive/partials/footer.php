4dc384c7fe74b47dfe5b912ddc1c1a0e20be67ed
Alex Muntada <alexm@alexm.org>
2018-06-05 12:59:46 +0200

    Credit goteo in footer

--- Resources/templates/responsive/partials/footer.php
+++ Resources/templates/responsive/partials/footer.php
@@ -1,14 +1,10 @@
-<footer id="footer" class="footer" style="display:none">
+<footer id="footer" class="footer">
     <div class="container-fluid">
       <div class="row">
 
           <div class="col-sm-5">
             <ul class="list-inline">
-              <li><a href="/about"><?= $this->text('regular-header-about')?></a></li>
-              <li><a href="http://stats.goteo.org" target="_blank"><?= $this->text('footer-resources-stats') ?></a></li>
-              <li><a href="/user/login"><?= $this->text('regular-login')?></a></li>
-              <li><a href="/contact"><?= $this->text('regular-footer-contact')?></a></li>
-              <li><a data-jslicense="1" href="/about/librejs">Licenses</a></li>
+              <li><a class="fa fa-github" target="_blank" href="https://github.com/GoteoFoundation/goteo"> Powered by goteo</a></li>
             </ul>
           </div>
 
